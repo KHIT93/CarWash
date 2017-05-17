9cd52b196c6497d707538b4ce2633810a5f29b0e
Daniel N <danielmlgaard@hotmail.com>
2017-05-17 17:27:57 +0200

    Baseline UI created

--- CarWash/Form1.Designer.cs
+++ CarWash/Form1.Designer.cs
@@ -28,24 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Bilvaks = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.Bilvaks.SuspendLayout();
+            this.tcCarWash = new System.Windows.Forms.TabControl();
+            this.lblCurrentStatus = new System.Windows.Forms.Label();
+            this.rdbtnSilverWash = new System.Windows.Forms.RadioButton();
+            this.progBarWash = new System.Windows.Forms.ProgressBar();
+            this.btnCancelWash = new System.Windows.Forms.Button();
+            this.btnStartWash = new System.Windows.Forms.Button();
+            this.txtInfoWash = new System.Windows.Forms.TextBox();
+            this.btnSendStatistic = new System.Windows.Forms.Button();
+            this.rdbtnStandardWash = new System.Windows.Forms.RadioButton();
+            this.rdbtnGoldWash = new System.Windows.Forms.RadioButton();
+            this.tabPage1.SuspendLayout();
+            this.tcCarWash.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // Bilvaks
-            // 
-            this.Bilvaks.Controls.Add(this.tabPage1);
-            this.Bilvaks.Controls.Add(this.tabPage2);
-            this.Bilvaks.Location = new System.Drawing.Point(12, 12);
-            this.Bilvaks.Name = "Bilvaks";
-            this.Bilvaks.SelectedIndex = 0;
-            this.Bilvaks.Size = new System.Drawing.Size(803, 642);
-            this.Bilvaks.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.txtInfoWash);
+            this.tabPage1.Controls.Add(this.btnSendStatistic);
+            this.tabPage1.Controls.Add(this.lblCurrentStatus);
+            this.tabPage1.Controls.Add(this.progBarWash);
+            this.tabPage1.Controls.Add(this.btnCancelWash);
+            this.tabPage1.Controls.Add(this.btnStartWash);
+            this.tabPage1.Controls.Add(this.rdbtnGoldWash);
+            this.tabPage1.Controls.Add(this.rdbtnSilverWash);
+            this.tabPage1.Controls.Add(this.rdbtnStandardWash);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -54,34 +62,136 @@
             this.tabPage1.Text = "Bilvask 1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // tcCarWash
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(795, 616);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Bilvask 2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tcCarWash.Controls.Add(this.tabPage1);
+            this.tcCarWash.Location = new System.Drawing.Point(12, 12);
+            this.tcCarWash.Name = "tcCarWash";
+            this.tcCarWash.SelectedIndex = 0;
+            this.tcCarWash.Size = new System.Drawing.Size(803, 642);
+            this.tcCarWash.TabIndex = 0;
+            // 
+            // lblCurrentStatus
+            // 
+            this.lblCurrentStatus.AutoSize = true;
+            this.lblCurrentStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurrentStatus.Location = new System.Drawing.Point(344, 372);
+            this.lblCurrentStatus.Name = "lblCurrentStatus";
+            this.lblCurrentStatus.Size = new System.Drawing.Size(51, 20);
+            this.lblCurrentStatus.TabIndex = 6;
+            this.lblCurrentStatus.Text = "label1";
+            this.lblCurrentStatus.Visible = false;
+            // 
+            // rdbtnSilverWash
+            // 
+            this.rdbtnSilverWash.AutoSize = true;
+            this.rdbtnSilverWash.Location = new System.Drawing.Point(31, 74);
+            this.rdbtnSilverWash.Name = "rdbtnSilverWash";
+            this.rdbtnSilverWash.Size = new System.Drawing.Size(69, 17);
+            this.rdbtnSilverWash.TabIndex = 1;
+            this.rdbtnSilverWash.Text = "Sølvvask";
+            this.rdbtnSilverWash.UseVisualStyleBackColor = true;
+            this.rdbtnSilverWash.CheckedChanged += new System.EventHandler(this.rdbtnSilverWash_CheckedChanged);
+            // 
+            // progBarWash
+            // 
+            this.progBarWash.Location = new System.Drawing.Point(207, 395);
+            this.progBarWash.Name = "progBarWash";
+            this.progBarWash.Size = new System.Drawing.Size(343, 31);
+            this.progBarWash.TabIndex = 5;
+            // 
+            // btnCancelWash
+            // 
+            this.btnCancelWash.Location = new System.Drawing.Point(300, 496);
+            this.btnCancelWash.Name = "btnCancelWash";
+            this.btnCancelWash.Size = new System.Drawing.Size(141, 64);
+            this.btnCancelWash.TabIndex = 4;
+            this.btnCancelWash.Text = "Anuller vask";
+            this.btnCancelWash.UseVisualStyleBackColor = true;
+            this.btnCancelWash.Click += new System.EventHandler(this.btnCancelWash_Click);
+            // 
+            // btnStartWash
+            // 
+            this.btnStartWash.Location = new System.Drawing.Point(300, 255);
+            this.btnStartWash.Name = "btnStartWash";
+            this.btnStartWash.Size = new System.Drawing.Size(141, 64);
+            this.btnStartWash.TabIndex = 3;
+            this.btnStartWash.Text = "Start vask";
+            this.btnStartWash.UseVisualStyleBackColor = true;
+            this.btnStartWash.Click += new System.EventHandler(this.btnStartWash_Click);
+            // 
+            // txtInfoWash
+            // 
+            this.txtInfoWash.Location = new System.Drawing.Point(207, 40);
+            this.txtInfoWash.Multiline = true;
+            this.txtInfoWash.Name = "txtInfoWash";
+            this.txtInfoWash.Size = new System.Drawing.Size(544, 113);
+            this.txtInfoWash.TabIndex = 8;
+            this.txtInfoWash.Visible = false;
+            // 
+            // btnSendStatistic
+            // 
+            this.btnSendStatistic.Location = new System.Drawing.Point(610, 496);
+            this.btnSendStatistic.Name = "btnSendStatistic";
+            this.btnSendStatistic.Size = new System.Drawing.Size(141, 64);
+            this.btnSendStatistic.TabIndex = 7;
+            this.btnSendStatistic.Text = "Send statistik";
+            this.btnSendStatistic.UseVisualStyleBackColor = true;
+            this.btnSendStatistic.Click += new System.EventHandler(this.btnSendStatistic_Click);
+            // 
+            // rdbtnStandardWash
+            // 
+            this.rdbtnStandardWash.AutoSize = true;
+            this.rdbtnStandardWash.Checked = true;
+            this.rdbtnStandardWash.Location = new System.Drawing.Point(31, 41);
+            this.rdbtnStandardWash.Name = "rdbtnStandardWash";
+            this.rdbtnStandardWash.Size = new System.Drawing.Size(94, 17);
+            this.rdbtnStandardWash.TabIndex = 0;
+            this.rdbtnStandardWash.TabStop = true;
+            this.rdbtnStandardWash.Text = "Standard vask";
+            this.rdbtnStandardWash.UseVisualStyleBackColor = true;
+            this.rdbtnStandardWash.CheckedChanged += new System.EventHandler(this.rdbtnStandardWash_CheckedChanged);
+            // 
+            // rdbtnGoldWash
+            // 
+            this.rdbtnGoldWash.AutoSize = true;
+            this.rdbtnGoldWash.Location = new System.Drawing.Point(31, 110);
+            this.rdbtnGoldWash.Name = "rdbtnGoldWash";
+            this.rdbtnGoldWash.Size = new System.Drawing.Size(70, 17);
+            this.rdbtnGoldWash.TabIndex = 2;
+            this.rdbtnGoldWash.Text = "Guldvask";
+            this.rdbtnGoldWash.UseVisualStyleBackColor = true;
+            this.rdbtnGoldWash.CheckedChanged += new System.EventHandler(this.rdbtnGoldWash_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(827, 666);
-            this.Controls.Add(this.Bilvaks);
+            this.Controls.Add(this.tcCarWash);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Bilvaks.ResumeLayout(false);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tcCarWash.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl Bilvaks;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabControl tcCarWash;
+        private System.Windows.Forms.TextBox txtInfoWash;
+        private System.Windows.Forms.Button btnSendStatistic;
+        private System.Windows.Forms.Label lblCurrentStatus;
+        private System.Windows.Forms.ProgressBar progBarWash;
+        private System.Windows.Forms.Button btnCancelWash;
+        private System.Windows.Forms.Button btnStartWash;
+        private System.Windows.Forms.RadioButton rdbtnGoldWash;
+        private System.Windows.Forms.RadioButton rdbtnSilverWash;
+        private System.Windows.Forms.RadioButton rdbtnStandardWash;
     }
 }
 
--- CarWash/Form1.cs
+++ CarWash/Form1.cs
@@ -12,9 +12,89 @@ namespace CarWash
 {
     public partial class Form1 : Form
     {
+        int selectedWash;
+
+        enum WashType
+        {
+            Standard = 1,
+            Silver = 2,
+            Gold = 3
+        };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void rdbtnStandardWash_CheckedChanged(object sender, EventArgs e)
+        {
+            btnStartWash.Enabled = true;
+            btnCancelWash.Enabled = true;
+            if (rdbtnStandardWash.Checked)
+            {
+                selectedWash = (int)WashType.Standard;
+                txtInfoWash.Visible = true;
+                txtInfoWash.Text = "Standardvask informationer";
+            }
+        }
+
+        private void rdbtnSilverWash_CheckedChanged(object sender, EventArgs e)
+        {
+            btnStartWash.Enabled = true;
+            btnCancelWash.Enabled = true;
+            if (rdbtnSilverWash.Checked)
+            {
+                selectedWash = (int)WashType.Silver;
+                txtInfoWash.Visible = true;
+                txtInfoWash.Text = "Sølvvask informationer";
+            }
+        }
+
+        private void rdbtnGoldWash_CheckedChanged(object sender, EventArgs e)
+        {
+            btnStartWash.Enabled = true;
+            btnCancelWash.Enabled = true;
+            if (rdbtnGoldWash.Checked)
+            {
+                selectedWash = (int)WashType.Gold;
+                txtInfoWash.Visible = true;
+                txtInfoWash.Text = "Guldvask informationer";
+            }
+        }
+
+        private void btnCancelWash_Click(object sender, EventArgs e)
+        {
+            //Anullering af vask
+
+            btnStartWash.Enabled = true;
+            rdbtnStandardWash.Enabled = true;
+            rdbtnSilverWash.Enabled = true;
+            rdbtnGoldWash.Enabled = true;
+        }
+
+        private void btnSendStatistic_Click(object sender, EventArgs e)
+        {
+            //Statistik af vasken, så kontoret kan lave rapport
+        }
+
+        private void btnStartWash_Click(object sender, EventArgs e)
+        {
+            //Start af vask
+
+            int SelectedWash = selectedWash;
+
+            btnStartWash.Enabled = false;
+            rdbtnStandardWash.Enabled = false;
+            rdbtnSilverWash.Enabled = false;
+            rdbtnGoldWash.Enabled = false;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnStartWash.Enabled = false;
+            btnCancelWash.Enabled = false;
         }
     }
 }
