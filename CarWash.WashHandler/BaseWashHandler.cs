f980bd696b13df6d2bcfb2906806db80a187b615
Dennis Asmussen <teamasmussen@hotmail.com>
2017-05-18 09:53:32 +0200

    Moved everything with handling to new project

---
+++ CarWash.WashHandler/BaseWashHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CarWash.Models.Programs;
+
+namespace CarWash
+{
+    public interface IBaseWashHandler
+    {
+        Task WashCarBase(int machineID);
+        Task WashCarBase(int machineID, CancellationToken ct);
+        Task WashCarBase(int machineID, CancellationToken ct, IProgress<BaseCarWash> progress);
+    }
+    class BaseWashHandler : IBaseWashHandler
+    {
+
+        public Task WashCarBase(int machineID)
+        {
+            return WashCarBase(machineID, CancellationToken.None);
+        }
+
+        public Task WashCarBase(int machineID, CancellationToken ct)
+        {
+            return WashCarBase(machineID, ct, new Progress<BaseCarWash>());
+        }
+
+        public Task WashCarBase(int machineID, CancellationToken ct, IProgress<BaseCarWash> progress)
+        {
+            return Task.Run(() =>
+            {
+
+            });
+        }
+    }
+}
--- CarWash.WashHandler/StandardWashHandler.cs
+++ CarWash.WashHandler/StandardWashHandler.cs
@@ -1,44 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using CarWash.Models.Programs;
 
-namespace CarWash
+namespace CarWash.WashHandler
 {
-    public interface IWashHandler
+    public interface IStandardWashHandler
     {
-        Task WashCarBase(int machineID);
-        Task WashCarBase(int machineID, CancellationToken ct);
-        Task WashCarBase(int machineID, CancellationToken ct, IProgress<BaseCarWash> progress);
-
         Task WashCarStandard(int machineID);
         Task WashCarStandard(int machineID, CancellationToken ct);
         Task WashCarStandard(int machineID, CancellationToken ct, IProgress<StandardCarWash> progress);
     }
-    class WashHandler : IWashHandler
+    class StandardWashHandler : IStandardWashHandler
     {
-
-        public Task WashCarBase(int machineID)
-        {
-            return WashCarBase(machineID, CancellationToken.None);
-        }
-
-        public Task WashCarBase(int machineID, CancellationToken ct)
-        {
-            return WashCarBase(machineID, ct, new Progress<BaseCarWash>());
-        }
-
-        public Task WashCarBase(int machineID, CancellationToken ct, IProgress<BaseCarWash> progress)
-        {
-            return Task.Run(() =>
-            {
-
-            });
-        }
-
         public Task WashCarStandard(int machineID)
         {
             return WashCarStandard(machineID, CancellationToken.None);
