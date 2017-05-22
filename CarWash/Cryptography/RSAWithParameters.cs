5204a285bf7156157274ac0fd23180a07f1ff507
Kenneth Hansen <kenneth@khansen-it.dk>
2017-05-22 13:17:17 +0200

    Add class to get data

---
+++ CarWash/Cryptography/RSAWithParameters.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace CarWash.Cryptography
+{
+	public class RSAWithParameterKey
+	{
+		private RSAParameters _publicKey;
+		private RSAParameters _privateKey;
+
+		public void AssignNewKey()
+		{
+			using (var rsa = new RSACryptoServiceProvider(2048))
+			{
+				rsa.PersistKeyInCsp = false;
+				_publicKey = rsa.ExportParameters(false);
+				_privateKey = rsa.ExportParameters(true);
+			}
+		}
+
+		public byte[] EncryptData(byte[] dataToEncrypt)
+		{
+			byte[] cipherbytes;
+
+			using (var rsa = new RSACryptoServiceProvider(2048))
+			{
+				rsa.PersistKeyInCsp = false;
+				rsa.ImportParameters(_publicKey);
+
+				cipherbytes = rsa.Encrypt(dataToEncrypt, true);
+			}
+
+			return cipherbytes;
+		}
+
+        public Task EncryptDataAsync(byte[] dataToEncrypt)
+        {
+            return Task.Run(() => this.EncryptData(dataToEncrypt));
+        }
+
+		public byte[] DecryptData(byte[] dataToDecrypt)
+		{
+			byte[] plain;
+
+			using (var rsa = new RSACryptoServiceProvider(2048))
+			{
+				rsa.PersistKeyInCsp = false;
+
+				rsa.ImportParameters(_privateKey);
+				plain = rsa.Decrypt(dataToDecrypt, true);
+			}
+
+			return plain;
+		}
+
+        public Task DecryptDataAsync(byte[] dataToDecrypt)
+        {
+            return Task.Run(() => this.DecryptData(dataToDecrypt));
+        }
+	}
+}
